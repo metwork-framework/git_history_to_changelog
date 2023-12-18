cd49b9ede2a2466e3dee5a72c9898358cc4c9ae7
thebaptiste <jean-baptiste.veslin@meteo.fr>
2023-12-18 14:55:42 +0000

    build: fix

--- ghtc/parser.py
+++ ghtc/parser.py
@@ -65,7 +65,7 @@ def parse(commit_message: str) -> Optional[ConventionalCommitMessage]:
             tmp1 = FOOTER_COMPILED_REGEX1.match(line)
             tmp2 = FOOTER_COMPILED_REGEX2.match(line)
             tmp3 = BREAKING_CHANGE_FOOTER_COMPILED_REGEX.match(line)
-            if len(footers) == 0 and tmp1 is None and tmp2 is None
+            if len(footers) == 0 and tmp1 is None and tmp2 is None \
                                  and tmp3 is None:
                 if body is None:
                     body = f"{line}"
